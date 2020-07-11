19e8ef08245a4e040f6608ea0bb2c3f3d081c403
fredhaus <frederikhausburg@gmail.com>
2020-07-11 15:00:53 +0200

    fixes cdm bg canvas

--- src/components/CenteredMenu.js
+++ src/components/CenteredMenu.js
@@ -1,22 +1,16 @@
 import React from "react";
 import { withRouter, Link } from "react-router-dom";
 
-class CenteredMenu extends React.Component {
-  state = {
-    menu1: "Curriculum Vitae".toUpperCase(),
-    menu2: "Projects".toUpperCase(),
-    menu3: "Contact".toUpperCase(),
-  };
+const CenteredMenu = () => {
 
-  render() {
     return (
       <div className="Aligner">
-        <Link to={'/cv'} className="menuFont">{this.state.menu1}</Link>
-        <Link href="#"className="menuFont">{this.state.menu2}</Link>
-        <Link href="#" className="menuFont">{this.state.menu3}</Link>
+        <Link to={'/cv'} className="menuFont">{"Curriculum Vitae".toUpperCase()}</Link>
+        <Link to={'/'} className="menuFont">{"Projects".toUpperCase()}</Link>
+        <Link to={'/'} className="menuFont">{"Contact".toUpperCase()}</Link>
       </div>
     );
   }
-}
+
 
 export default withRouter(CenteredMenu);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, Switch, Route } from "react-router-dom";
 import axios from "axios";
 import { withRouter } from "react-router-dom";
 import CenteredMenu from "./CenteredMenu";
+import useWindowDimensions from "../Hooks/useWindowDImensions"
 
 const Home = (props) => {
-  const [rgb, setRgb] = useState({
-    x: "",
-    y: "",
+
+  const { height, width } = useWindowDimensions();
+
+  const [mousePos, setMousePos] = useState({
+    x: width / 2,
+    y: height / 2,
   });
 
   const [pos, setPos] = useState({
-    canvasX: 500,
-    canvasY: 500,
+    canvasX: width,
+    canvasY: height,
   });
 
   // changeHandler = (event) => {
@@ -23,6 +27,63 @@ const Home = (props) => {
   //   });
   // };
 
+  const drawBG = () => {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    let dist = 0;
+    ctx.clearRect(0, 0, pos.canvasX, pos.canvasY);
+    ctx.beginPath();
+    ctx.setLineDash([2, 10]);
+    ctx.lineWidth = 2;
+    ctx.moveTo(0, 0);
+    ctx.lineTo(mousePos.x - dist, mousePos.y  - dist);
+    ctx.lineTo(pos.canvasX, 0);
+    let grd = ctx.createLinearGradient(pos.canvasX/2, 0, mousePos.x, (mousePos.y -100));
+    grd.addColorStop(0, "#A9A9A9");
+    grd.addColorStop(1, "white");
+    ctx.fillStyle = grd;
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    let grd2 = ctx.createLinearGradient(
+      pos.canvasX,
+      pos.canvasX,
+      mousePos.x,
+      mousePos.y 
+    );
+    grd2.addColorStop(0, "#2F4F4F");
+    grd2.addColorStop(1, "white");
+    ctx.lineTo(pos.canvasX, 0);
+    ctx.lineTo(pos.canvasX, pos.canvasY);
+    ctx.lineTo(pos.canvasX / 2, pos.canvasY);
+    ctx.lineTo(mousePos.x, mousePos.y );
+    ctx.closePath();
+    ctx.fillStyle = grd2;
+    ctx.fill();
+    ctx.beginPath();
+    let grd3 = ctx.createLinearGradient(
+      0,
+      pos.canvasX,
+      mousePos.x,
+      mousePos.y 
+    );
+    grd3.addColorStop(0, "#696969");
+    grd3.addColorStop(1, "white");
+    ctx.moveTo(0, pos.canvasY);
+    ctx.lineTo(pos.canvasX / 2, pos.canvasY);
+    ctx.lineTo(mousePos.x, mousePos.y  - dist);
+    ctx.lineTo(0, 0);
+    ctx.closePath();
+    ctx.fillStyle = grd3;
+    ctx.fill();
+  }
+
+
+  useEffect(() => {
+    drawBG()
+  }, [])
+
+
   const onMouseMove2 = (e) => {
     let w = window,
       d = document,
@@ -31,82 +92,15 @@ const Home = (props) => {
       screenWidth = w.innerWidth || e.clientWidth || g.clientWidth,
       screenHeight = w.innerHeight || e.clientHeight || g.clientHeight;
 
-    if (pos.canvasX != screenWidth || pos.canvasY != screenHeight) {
+    if (pos.canvasX !== screenWidth || pos.canvasY !== screenHeight) {
       setPos({ canvasX: screenWidth, canvasY: screenHeight });
     }
 
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
+    setMousePos({ x: e.screenX, y: e.screenY-135 })
+    drawBG()
 
-    let dist = 0;
-
-    ctx.clearRect(0, 0, pos.canvasX, pos.canvasY);
-    ctx.beginPath();
-    ctx.setLineDash([2, 10]);
-    ctx.lineWidth = 2;
-    ctx.moveTo(0, 0);
-    ctx.lineTo(e.screenX - dist, e.screenY - 100 - dist);
-    ctx.lineTo(pos.canvasX, 0);
-    let grd = ctx.createLinearGradient(0, 0, 0, (e.screenY - 100) * 0.8);
-    grd.addColorStop(0, "#A9A9A9");
-    grd.addColorStop(1, "white");
-    ctx.fillStyle = grd;
-    ctx.fill();
-    ctx.closePath();
-
-    ctx.beginPath();
-    let grd2 = ctx.createLinearGradient(
-      pos.canvasX,
-      pos.canvasX,
-      e.screenX,
-      e.screenY - 100
-    );
-    grd2.addColorStop(0, "#2F4F4F");
-    grd2.addColorStop(1, "white");
-    ctx.lineTo(pos.canvasX, 0);
-    ctx.lineTo(pos.canvasX, pos.canvasY);
-    ctx.lineTo(pos.canvasX / 2, pos.canvasY);
-    ctx.lineTo(e.screenX, e.screenY - 100);
-    ctx.closePath();
-    ctx.fillStyle = grd2;
-    ctx.fill();
-
-    ctx.beginPath();
-    let grd3 = ctx.createLinearGradient(
-      0,
-      pos.canvasX,
-      e.screenX,
-      e.screenY - 100
-    );
-    grd3.addColorStop(0, "#696969");
-    grd3.addColorStop(1, "white");
-    ctx.moveTo(0, pos.canvasY);
-    ctx.lineTo(pos.canvasX / 2, pos.canvasY);
-    ctx.lineTo(e.screenX, e.screenY - 100 - dist);
-    ctx.lineTo(0, 0);
-    ctx.closePath();
-    ctx.fillStyle = grd3;
-    ctx.fill();
-
-    // ctx.lineTo(e.screenX + dist, e.screenY - 100 - dist);
-
-    // ctx.lineTo(e.screenX, e.screenY - 100 + dist);
-    // ctx.strokeStyle = "#FF0000";
-    // ctx.stroke();
-
-    // ctx.clearRect(0, 0, pos.canvasX, pos.canvasY);
-    // ctx.beginPath();
-    // ctx.setLineDash([2, 10]);
-    // ctx.lineWidth = 2;
-    // ctx.moveTo(0, 0);
-    // ctx.lineTo(e.screenX - dist, e.screenY - 100 - dist);
-    // ctx.moveTo(pos.canvasX, 0);
-    // ctx.lineTo(e.screenX + dist, e.screenY - 100 - dist);
-    // ctx.moveTo(pos.canvasX / 2, pos.canvasY);
-    // ctx.lineTo(e.screenX, e.screenY - 100 + dist);
-    // ctx.strokeStyle = "#FF0000";
-    // ctx.stroke();
   };
+
 
   const content = (
     <Switch>
@@ -133,17 +127,17 @@ const Home = (props) => {
         path="/cv"
         render={() => (
           <div onMouseMove={onMouseMove2}>
-          <div className="homeBG">
-            <canvas
-              className="canvas"
-              id="myCanvas"
-              width={`${pos.canvasX}`}
-              height={`${pos.canvasY}`}
-            ></canvas>
-                    <Link to={'/'} className="menuFont">Home</Link>
+            <div className="homeBG">
+              <canvas
+                className="canvas"
+                id="myCanvas"
+                width={`${pos.canvasX}`}
+                height={`${pos.canvasY}`}
+              ></canvas>
+              <Link to={'/'}>Home</Link>
 
-            <h2>Frederik Hausburg</h2>
-          </div>
+              <h2>Frederik Hausburg</h2>
+            </div>
           </div>
         )}
       ></Route>
