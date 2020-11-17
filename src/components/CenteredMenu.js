281c64b7082bdb35ac6ff126c3c342902cb2172e
Fred Haus <fredhaus@Freds-MacBook-Pro-235.local>
2020-11-17 15:19:40 +0100

    adds projects to home view

--- src/components/CenteredMenu.js
+++ src/components/CenteredMenu.js
@@ -2,28 +2,83 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const CenteredMenu = () => {
-  const [contact, setContact] = useState(false);
+  const [contact, setContact] = useState(true);
   const [mail, setMail] = useState(false);
-
-  let toggleClassName = "hidden";
+  const [dressMeUpDetailVis, setDressMeUpDetailVis] = useState(false);
+  const [metaDomainDetailVis, setMetaDomainDetailVis] = useState(false);
+  const [evi2DetailVis, setEvi2DetailVis] = useState(false);
 
   const toggle = () => {
     setContact(!contact);
-    !contact ? (toggleClassName = "hidden") : (toggleClassName = "visible");
   };
 
   const toggleMail = () => {
     setMail(!mail);
   };
 
+  const toggleDressMeUpDetailVis = () => {
+    setDressMeUpDetailVis(!dressMeUpDetailVis);
+  };
+
+  const toggleMetaDomainDetailVis = () => {
+    setMetaDomainDetailVis(!metaDomainDetailVis);
+  };
+
+  const toggleEvi2DetailVis = () => {
+    setEvi2DetailVis(!evi2DetailVis);
+  };
+
   return (
     <div className="Aligner">
-      <Link to={"/cv"} className="menuFont">
-        {"Curriculum Vitae".toUpperCase()}
-      </Link>
-      <Link to={"/projects"} className="menuFont">
-        {"Projects".toUpperCase()}
-      </Link>
+      <div className="AlignerTop" style={{margin: "25px"}}>
+        {dressMeUpDetailVis || metaDomainDetailVis || evi2DetailVis ? (
+          dressMeUpDetailVis ? (
+            <span className="detailsFont">
+             <b>Random Outfit generator</b> - React, Express.js
+            </span>
+          ) : metaDomainDetailVis ? (
+            <span className="detailsFont"><b>Domain meta search engine</b> - Express.js</span>
+          ) : (
+            <span className="detailsFont"><b>MVP self-evaluating tool for dutch schools</b> - React, Express.js</span>
+          )
+        ) : (
+          <span style={{ opacity: "0%" }} className="detailsFont">
+            0%
+          </span>
+        )}
+      </div>
+
+      <a
+        target="_blank"
+        href="https://dressmeupcc.herokuapp.com/"
+        className="menuFont"
+        onMouseOver={toggleDressMeUpDetailVis}
+        onMouseLeave={toggleDressMeUpDetailVis}
+      >
+        DRESS_ME_UP
+      </a>
+      <a
+        target="_blank"
+        href="https://metadomain2.herokuapp.com/"
+        className="menuFont"
+        onMouseOver={toggleMetaDomainDetailVis}
+        onMouseLeave={toggleMetaDomainDetailVis}
+      >
+        META_DOMAIN
+      </a>
+      <a
+        target="_blank"
+        href="https://evi2.herokuapp.com/"
+        className="menuFont"
+        onMouseOver={toggleEvi2DetailVis}
+        onMouseLeave={toggleEvi2DetailVis}
+      >
+        EVI_2
+      </a>
+      <span style={{opacity: "0%", margin: "25px"}}>
+      
+      </span>
+
       {!contact ? (
         <span onClick={toggle} className="menuFont">
           {"Contact".toUpperCase()}
@@ -45,13 +100,13 @@ const CenteredMenu = () => {
             <a onClick={toggleMail}>
               <img
                 src="/icon_mail.png"
-                alt="frederik@hausburg.net"
+                alt="frederikhausburg@gmail.com"
                 className="contactIcon"
               />
             </a>
           ) : (
-            <span onClick={toggleMail} className="menuFont">
-              {"Frederik@Hausburg.net".toUpperCase()}
+            <span onClick={toggleMail} className="mailFont">
+              {"frederikhausburg@gmail.com".toUpperCase()}
             </span>
           )}
         </div>
